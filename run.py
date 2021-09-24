07415f09027cc5f8aaa99a63e2216233dbf90246
CarlssonOscar <oosccar@gmail.com>
2021-09-24 08:57:59 +0000

    Get attack input from player, convert coordinates to integers, create board and add attacks location to board

--- run.py
+++ run.py
@@ -1,3 +1,29 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def render(width, height, attack):
+    header = "#" + "-" * width + "+"
+    print(header)
+
+    attack_set = set(attack)
+    for y in range(height):
+        row = []
+        for x in range(width):
+            if (x,y) in attack_set:
+                add = "X"
+            else:
+                add = " "
+            row.append(add)
+        print("|" + "".join(row) + "|")
+
+    print(header)
+
+if __name__ == "__main__":
+    attack = []
+
+    while True:
+        inp = input("Where do you wish to attack?\n")
+        xstr, ystr = inp.split(",")
+        x = int(xstr)
+        y = int(ystr)
+
+        attack.append((x,y))
+        render(10, 10, attack)
+
